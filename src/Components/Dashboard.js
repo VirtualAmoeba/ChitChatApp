32c9e50558db932c1fc5ebb84111b6c21d342ed1
Armoni Tigner <atigner2@student.gsu.edu>
2021-11-12 15:51:43 -0500

    added styling to dashbaord

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,4 +1,5 @@
 import React from "react"
+import pic from "./profileimages/Unknown.jpg"
 export default function Dashboard(){
     return(
         <div className = "ContainerDash">
@@ -16,34 +17,48 @@ export default function Dashboard(){
             </div>
                 <div className= "cont">
                     <div>
-                        <p className = "primarycont"> Primary Contacts</p>
+                        <div>
+                            <p className = "primarycont"> Primary Contacts</p>
+                        </div>
                     </div>
-                    <div className= "favcont">
-                        <div id= "cir1">Armoni</div>
-                        <div id= "cir2">Al</div>
-                        <div id= "cir3">Serina</div>
-                        <div id= "cir4">Deborah</div>
-                        <div id= "cir5">Troy</div>
-                        <div id= "cir6">Ashley</div>
-                        <div id= "cir7">Jermey</div>
-                        <div id= "cir8">Manny</div>
+                    <div>
+                        <div className= "favcont">
+                            <div id= "cir1">Armoni</div>
+                            <div id= "cir2">Al</div>
+                            <div id= "cir3">Serina</div>
+                            <div id= "cir4">Deborah</div>
+                            <div id= "cir5">Troy</div>
+                            <div id= "cir6">Ashley</div>
+                            <div id= "cir7">Jermey</div>
+                            <div id= "cir8">Manny</div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className = "myProfile">
+                    <img src = {pic} className = "myProfile"></img>
+                    <div>
+                        <p> My Username: Armoni81</p>
+                        <p>My Email: atigner81@gmail.com</p>
+                        <p>My Number: 6789991212</p>
+                        <p>About me : </p>
+                    </div>
+                    
 
-                    </div>
+
                 </div>
                 <div className= "mess">
-                <p>Messages</p>
-                </div>
-            <div className = "messageList">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
+                    <div>
+                    <p className= "latestmess"> Latest Messages</p>
+                    </div>
+                </div>  
+                    <div className = "footerbtn">
+                        <button className="newmessbtn">Home</button>
+                        <button className="newmessbtn">New message</button>
+                        <button className="newmessbtn">My Profile</button>
+                        <button className="newmessbtn">Sign Out</button>
+                    </div>
+                
         </div> 
                           
        
