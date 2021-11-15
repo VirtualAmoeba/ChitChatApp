99311c254ab7449adb8821c2b4055b8ce6a3bc8a
Serina J <88992204+VirtualAmoeba@users.noreply.github.com>
2021-11-15 09:08:14 -0500

    Updated Landing Page
    Updates include motion background. Icons and sections. WIll be updating more today

---
+++ src/Components/Contacts.js
@@ -0,0 +1,24 @@
+<div>
+    {src && (
+        <Image
+        circular
+        width={45}
+        heigth={45}
+        src={src}
+        style={style}
+        className={className}
+        />
+    )}
+
+    {!src && (
+        <div style={style} className={`thumbnail ${className}`}>
+        <span>{getInitials}()}</span>
+        </div>
+        )}
+    </div>
+    );
+};
+
+</div>
+
+export default ImageThumb;
