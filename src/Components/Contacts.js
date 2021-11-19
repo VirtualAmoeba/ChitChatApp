a3425fe7efe54fbbec16538b1a714e8613b4bc08
deb <lellowgill@gmail.com>
2021-11-19 11:13:13 -0500

    copied and paste work from github

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -1,35 +1,53 @@
-/*<div>
-    {src && (
-        <Image
-        circular
-        width={45}
-        heigth={45}
-        src={src}
-        style={style}
-        className={className}
-        />
-    )}
-
-    {!src && (
-        <div style={style} className={`thumbnail ${className}`}>
-        <span>{getInitials}()}</span>
-        </div>
-        )}
-    </div>
-    );
-};
-
-</div>
-
-export default ImageThumb;
 import React from 'react';
+import { collection, query, getDocs, setDoc, doc } from "firebase/firestore";
+import { db } from "../App";
 
-export default function Contacts(){
-    return(
-        <div>
-            404 Page Not Found
-        </div>
-    )
+export class Contacts extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            fullName: "",
+            phoneNumber: ""
+        }
+        this.addFullName = this.addFullName.bind(this)
+        this.addPhoneNumber = this.addPhoneNumber.bind(this)
+        this._addContact = this._addContact.bind(this)
+    }
+
+    render () {
+        return(
+            <div>
+                <input onChange = {this.addFullName}></input>
+                <input onChange = {this.addPhoneNumber}></input>
+                <button onClick = {this._addContact}>Send</button>
+            </div>
+        )
+    }
+
+    
+    addFullName(event){
+        this.setState({fullName:event.target.value})
+    }
+    addPhoneNumber(event){
+        this.setState({phoneNumber:event.target.value})
+    }
+
+    _addContact(){
+        setDoc(doc(db,"contacts", "User"),{
+            fullName: this.state.fullName,
+            phoneNumber:this.state.phoneNumber,
+        })
+        this.setState({
+            fullName: "",
+            phoneNumber:""
+        })
+    }
 }
 
-
+// componentDidMount() ;{
+//     const q = query(collection(this.props.DB, "contacts"));
+//     getDocs(q).then((querySnapshot) => {
+//       this._getContacts(querySnapshot)
+//     })
+//   }
